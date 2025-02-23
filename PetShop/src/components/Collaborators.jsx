942046e02f73e13fcb1a96de75fc2cfe8f2b5445
Ivaneudo <159584361+Ivaneudo@users.noreply.github.com>
2025-02-23 13:48:28 -0300

    esse vai ficar para outro momento

--- PetShop/src/components/Collaborators.jsx
+++ PetShop/src/components/Collaborators.jsx
@@ -1,6 +1,68 @@
 import Coll from "./Collaboratores.module.css"
+import { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
+const url = "http://localhost:3000/caixa"
 
 export const Collaborators = () => {
+
+  // const [caixa, setCaixa] = useState("")
+  // const [name, setName] = useState("")
+  // const [senha, setSenha] = useState("")
+
+  // useEffect(() => {
+  //   async function getData() {
+  //     const res = await fetch(url)
+
+  //     const data = await res.json()
+
+  //     setCaixa(data)
+  //   }
+
+  //   getData()
+  // }, [])
+
+  function submi(e) {
+
+    e.preventDefault([])
+
+    let user = document.querySelector('#userColl').value;
+    let password = +document.querySelector('#passColl').value
+
+    if(user === "") return alert('Adiciona seu cpf no campo user')
+
+    if(password === 0  || password === "") {
+      return alert("Adicione a sua senha no campo password")
+    } else {
+
+      const [caixa, setCaixa] = useState("")
+  
+      useEffect(() => {
+        async function getData() {
+          const res = await fetch(url)
+    
+          const data = await res.json()
+    
+          setCaixa(data)
+          
+          caixa.map((caixa) => {
+            if (caixa.cpf === user && caixa.senha === password) {
+              console.log("passou!")
+            } else {
+              console.log("senha ou user incorretos")
+            }
+          })
+          
+        }
+    
+        getData()
+      }, [caixa])
+    
+    }
+
+
+  }
+
   return (
     <div className='container'>
 
@@ -13,14 +75,18 @@ export const Collaborators = () => {
             <form action="">
                 <input 
                 type="text" 
-                placeholder='User: '/>
+                placeholder='User: '
+                id="userColl"
+                maxLength="14"/>
                 <input 
                 type="text" 
-                placeholder='Passwold: '/>
+                placeholder='Passwold: '
+                id="passColl"/>
                 <input 
                 type="submit" 
                 value="Entrar"
-                className={Coll.Enter} />
+                className={Coll.Enter}
+                onClick={submi} />
             </form> 
         </div>
     </div>
